9d270b47c2a58dc4372777546b3d3c20a811aa77
aluamark <aluamark@gmail.com>
2024-10-16 06:50:13 +0800

    Added simple loading for bgImage in hero section to load, removed red background when sidebar is open in larger screen

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 export default function Hero() {
   const [tiltStyle, setTiltStyle] = useState({});
+  const [isImageLoading, setIsImageLoading] = useState(true);
   const containerRef = useRef(null);
 
   useEffect(() => {
@@ -28,8 +29,37 @@ export default function Hero() {
     };
   }, []);
 
+  const handleImageLoad = () => {
+    setIsImageLoading(false);
+  };
+
   return (
     <section className="min-h-screen flex justify-center items-center relative overflow-hidden">
+      {isImageLoading && (
+        <div className="absolute inset-0 flex justify-center items-center bg-neutral-900 z-50">
+          <svg
+            className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
+      )}
+
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{ backgroundImage: "url('/kaboompics.jpg')" }}
@@ -65,7 +95,13 @@ export default function Hero() {
             <span className="absolute pt-1 left-1/2 -translate-x-1/2 text-base font-semibold leading-6">
               Hystoria
             </span>
-            <Image src="/qrcode.svg" alt="qr-code" width={296} height={296} />
+            <Image
+              src="/qrcode.svg"
+              alt="qr-code"
+              width={296}
+              height={296}
+              onLoadingComplete={handleImageLoad}
+            />
           </div>
         </div>
       </div>
